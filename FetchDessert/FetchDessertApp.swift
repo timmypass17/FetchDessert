12d53373f45ec5fbf9fcf68a0dfd0690b2b10404
Timmy Nguyen <timmypass17@gmail.com>
2023-09-12 17:45:45 -0700

    Initial Commit

---
+++ FetchDessert/FetchDessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertApp.swift
+//  FetchDessert
+//
+//  Created by Timmy Nguyen on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
